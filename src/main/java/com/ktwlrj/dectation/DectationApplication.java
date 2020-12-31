101d485d6e12264a1db99e1425ce0672277517f6
shutao <shutao@ktwlrj.com>
2020-12-31 12:10:39 +0800

    初始化

---
+++ src/main/java/com/ktwlrj/dectation/DectationApplication.java
@@ -0,0 +1,13 @@
+package com.ktwlrj.dectation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DectationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DectationApplication.class, args);
+    }
+
+}
