f3a33247e7d70b0f37a5ea87017d02fab7fff3cf
shutao <shutao@ktwlrj.com>
2020-12-31 12:07:43 +0800

    初始化

---
+++ src/test/java/com/ktwlrj/dectation/DectationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktwlrj.dectation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DectationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
